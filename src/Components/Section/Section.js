d030cce07a968f41b6eb16d966c3ce6ce9714ecb
Anh Nguyen <theanhnguyen0502@gmail.com>
2022-10-04 07:46:11 +0700

    04/10/2022

--- src/Components/Section/Section.js
+++ src/Components/Section/Section.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import Fade from 'react-reveal/Fade';
+import Reveal from 'react-reveal/Reveal';
 
 import styles from './Section.module.scss';
 import downArrow from '~/assets/images/down-arrow.svg';
@@ -10,22 +11,24 @@ const Section = ({ backgroundImg, title, desc, leftBtnTxt, rightBtnTxt }) => {
     const background = require(`../../assets/images/${backgroundImg}`);
     return (
         <div className={cx('wrapper')} style={{ backgroundImage: `url('${background}')` }}>
-            <Fade>
+            <Fade bottom>
                 <div className={cx('text')}>
                     <h1>{title}</h1>
                     <p>{desc}</p>
                 </div>
             </Fade>
             <div className={cx('btn-group')}>
-                <Fade>
-                    <div className={cx('btns')}>
+                <div className={cx('btns')}>
+                    <Fade left delay="1000">
                         <button className={cx('left-btn')}>{leftBtnTxt}</button>
+                    </Fade>
+                    <Fade right delay="1000">
                         {rightBtnTxt && <button className={cx('right-btn')}>{rightBtnTxt}</button>}
-                    </div>
-                    <div className={cx('arrow-container')}>
-                        <img className={cx('down-arrow')} src={downArrow} alt="down-arrow" />
-                    </div>
-                </Fade>
+                    </Fade>
+                </div>
+                <div className={cx('arrow-container')}>
+                    <img className={cx('down-arrow')} src={downArrow} alt="down-arrow" />
+                </div>
             </div>
         </div>
     );
